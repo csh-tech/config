80694fdd2cbd58b62041b2594866db140094542a
chenshanhui <1065521866@qq.com>
2019-12-01 14:18:27 +0800

    fix some bugs in conf.sh

--- conf.sh
+++ conf.sh
@@ -1,17 +1,25 @@
 #! /bin/sh
 
+# git clone ssh://git@116.196.65.235:8822/csh/config.git
+cd config
+tar -xzvf conf/conf.tar.gz
+
 # vim config
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+cp -r conf/vimrc ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # zsh config
 apt install zsh
 chsh -s $(which zsh)
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp -r config/conf/oh-my-zsh ~/.oh-my-zsh
 cp ~/.zshrc ~/.zshrc.orig
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 # tmux config
 apt install tmux
-git clone --recursive https://github.com/tony/tmux-config.git ~/.tmux
+cp -r config/conf/tmux-config ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+
+cd ~
+rm -rf config
+
